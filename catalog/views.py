74d068e49ab7c5e82753034e3bf788145a3f8822
kanlar75 <kanlar75@gmail.com>
2023-08-13 16:26:34 +0300

    добавлены urls и viewers

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
