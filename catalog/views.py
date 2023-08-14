ff0b7f4b87fb0c44e182e96831c5345079b12f75
kanlar75 <kanlar75@gmail.com>
2023-08-14 19:10:01 +0300

    new home work

--- catalog/views.py
+++ catalog/views.py
@@ -4,8 +4,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
     return render(request, 'catalog/home.html')
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
     return render(request, 'catalog/contacts.html')
