d0415911e7876cbac7001a21e3a9d482292c41bf
kanlar75 <kanlar75@gmail.com>
2023-08-21 18:40:52 +0300

    ДЗ 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import *
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/', products, name='products')
     ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,28 @@
 # Create your views here.
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
-def home(request):
+def index(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/index.html', {'title': 'Главная страница'})
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
-    return render(request, 'catalog/contacts.html')
+    # if request.method == 'POST':
+    #     name = request.POST.get('name')
+    #     email = request.POST.get('phone')
+    #     message = request.POST.get('message')
+    #     print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/contacts.html', {'title': 'Контакты'})
+
+
+def products(request):
+    products_l = Product.objects.all()
+    context = {'products': products_l, 'title': 'Продукты'}
+    return render(request, 'catalog/products.html', {'products': products_l, 'title': 'Продукты'})
