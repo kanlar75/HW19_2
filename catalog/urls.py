4ac4ab89be2b05c498a5c76343b6efa5e38c8403
kanlar75 <kanlar75@gmail.com>
2023-08-23 12:27:54 +0300

    ДЗ 20.2 доработки (задание 1 и добавлен в git static)

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,9 +3,11 @@ from catalog.apps import CatalogConfig
 from catalog.views import *
 
 app_name = CatalogConfig.name
+
 urlpatterns = [
     path('', index, name='index'),
     path('index/', index, name='index'),
     path('contacts/', contacts, name='contacts'),
     path('products/', products, name='products'),
+    path('product/<int:pk>', product, name='product'),
     ]
